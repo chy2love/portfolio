2a02bb9c6553a675a8c56e7e508c7dd06993e75b
chy2love <chlgkduddlek@gmail.com>
2023-02-17 21:08:52 +0900

    Test: scroll anime

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
 import React, { useRef, useEffect, useState, useCallback } from 'react';
 import styled from 'styled-components';
-import canvasVideo from '../src/assets/canvasVideo.mp4';
+import canvasVideo from '../src/assets/canvasVideo_10s.mp4';
 export default function App() {
   const [videoTime, setVideoTime] = useState(0);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const videoRef = useRef<HTMLVideoElement>(null);
   const ctx = canvasRef.current?.getContext('2d');
 
-  const video = videoRef.current;
   useEffect(() => {
-    if (video) {
-      console.log('hi');
-      video.currentTime = videoTime;
+    if (videoRef.current) {
+      const video = videoRef.current;
       const render = () => {
-        ctx?.drawImage(video, 0, 0, 300, 100);
+        console.log(video);
+        video.currentTime = videoTime;
+        // ctx?.drawImage(video, 0, 0, 300, 100);
       };
-      requestAnimationFrame(render);
+      render();
+      // requestAnimationFrame(render);
     }
   }, [videoTime]);
 
-  const handleScroll = () => {
-    console.log(scrollY);
-    setVideoTime(scrollY / 1000);
-  };
+  const handleScroll = useCallback(() => {
+    if (videoRef.current) {
+      console.log(
+        Math.round((videoRef.current.duration / 1400) * scrollY * 100) / 100
+      );
+      setVideoTime(
+        Math.round((videoRef.current.duration / 1400) * scrollY * 100) / 100
+      );
+    }
+  }, [videoRef.current]);
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => {
@@ -30,20 +37,24 @@ export default function App() {
     };
   }, []);
   return (
-    <div>
+    <VideoContainer>
       <Video ref={videoRef} src={canvasVideo} muted></Video>
-      <Canvas ref={canvasRef}></Canvas>
-    </div>
+      {/* <Canvas ref={canvasRef}></Canvas> */}
+    </VideoContainer>
   );
 }
-const Video = styled.video`
-  display: none;
-  width: 100vw;
-  height: 100vh;
-`;
-const Canvas = styled.canvas`
+const VideoContainer = styled.div`
   position: fixed;
-  background-repeat: no-repeat;
-  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 100vh;
 `;
+const Video = styled.video`
+  /* display: none; */
+`;
+// const Canvas = styled.canvas`
+//   position: fixed;
+//   width: 1240px;
+//   height: 753px;
+// `;
