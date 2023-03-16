0db96e406ecf895f3ee3fa2c1acc77c7f5406594
chy2love <chlgkduddlek@gmail.com>
2023-03-16 18:00:29 +0900

    23.03.16 Update: Spinner

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import ScrollToTop from './components/ScrollToTop';
+import Spinner from './components/Spinner';
 export default function App() {
   return (
     <>
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import BGAnime from '../components/Homepage/BGAnime';
 import ContactMe from '../components/Homepage/ContactMe';
 import FAQArea from '../components/Homepage/FAQArea';
 import IntroArea from '../components/Homepage/IntroArea';
 import ProjectArea from '../components/Homepage/ProjectArea';
 import SkillsArea from '../components/Homepage/SkillsArea';
+import Spinner from '../components/Spinner';
 export default function Homepage() {
+  const [spinnerActive, setSpinnerActive] = useState(true);
+  const [params, setParams] = useSearchParams();
+  const navigate = useNavigate();
+  useEffect(() => {
+    window.onbeforeunload = function (e) {
+      e.preventDefault();
+      console.log('넌 못지나간다');
+      return navigate('/?');
+    };
+  }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSpinnerActive(false);
+    }, 2500);
+    timer;
+    return () => {
+      timer;
+    };
+  }, []);
   return (
     <>
+      {!params.get('link') && spinnerActive && <Spinner />}
       <BGAnime />
       <IntroArea />
       <ProjectArea />
